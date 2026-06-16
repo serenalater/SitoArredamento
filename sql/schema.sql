8f5505bd0c9e9f88522cc2a4ed0e6ba2630478a6
serenalater <serena.late@gmail.com>
2026-06-16 10:03:13 +0200

    modifiche

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,7 +9,7 @@ CREATE DATABASE IF NOT EXISTS mentoring_app
 USE mentoring_app;
 
 
--- Tabella Utenti
+-- Tabella Utenti (CLIENTI E ADMIN)
 
 CREATE TABLE IF NOT EXISTS users (
 
@@ -21,3 +21,62 @@ CREATE TABLE IF NOT EXISTS users (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 ) ENGINE=InnoDB;
+
+
+
+--Products
+
+CREATE TABLE IF NOT EXISTS products(
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    is_active BOOLEAN NOT NULL DEFAULT TRUE,
+    category VARCHAR(255) DEFAULT NULL,
+    file_path VARCHAR(255) DEFAULT NULL,
+    image_path VARCHAR(255) DEFAULT NULL,
+    stock_quantity INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+) ENGINE=InnoDB;
+
+
+--Orders
+
+CREATE TABLE IF NOT EXISTS orders(
+
+    id INT PRIMARY KEY
+    user_id INT FK
+    total_amount DECIMAL(10,2)
+    status ENUM('pending','paid', 'completed','cancelled')
+    payment_ref VARCHAR(255)
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+) ENGINE=InnoDB;
+
+
+-- order items
+CREATE TABLE IF NOT EXISTS order item (
+    id INT PRIMARY KEY
+    order_id INT FK
+    product_id INT FK
+    quantity INT
+    unit_price DECIMAL(10,2)
+    subtotal DECIMAL(10,2)
+
+)
+
+-- installations
+CREATE TABLE IF NOT EXISTS order installations (
+
+    id INT PRIMARY KEY
+    order_id INT FK
+    technician_id INT FK
+    scheduled_at DATETIME
+    address VARCHAR(255)
+    status ENUM('confirmed','completed','cancelled')
+    notes TEXT
+    installation_price DECIMAL(10,2)
+    created_at TIMESTAMP
+)
