a0b182526677f814b98d635545bf71ebe6acf264
serenalater <serena.late@gmail.com>
2026-06-16 10:17:06 +0200

    MODIFICHE

--- sql/schema.sql
+++ sql/schema.sql
@@ -46,37 +46,39 @@ CREATE TABLE IF NOT EXISTS products(
 
 CREATE TABLE IF NOT EXISTS orders(
 
-    id INT PRIMARY KEY
-    user_id INT FK
-    total_amount DECIMAL(10,2)
-    status ENUM('pending','paid', 'completed','cancelled')
-    payment_ref VARCHAR(255)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    total_amount DECIMAL(10,2),
+    status ENUM('pending','paid', 'completed','cancelled'),
+    payment_ref VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 
 ) ENGINE=InnoDB;
 
 
 -- order items
-CREATE TABLE IF NOT EXISTS order item (
-    id INT PRIMARY KEY
-    order_id INT FK
-    product_id INT FK
-    quantity INT
-    unit_price DECIMAL(10,2)
-    subtotal DECIMAL(10,2)
+CREATE TABLE IF NOT EXISTS order_item (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2),
+    subtotal DECIMAL(10,2),
+    FOREIGN KEY (order_id) REFERENCES orders(id),
+    FOREIGN KEY (product_id) REFERENCES products(id)
 
-)
+)ENGINE=InnoDB;
 
 -- installations
-CREATE TABLE IF NOT EXISTS order installations (
+CREATE TABLE IF NOT EXISTS order_installations (
 
-    id INT PRIMARY KEY
-    order_id INT FK
-    technician_id INT FK
-    scheduled_at DATETIME
-    address VARCHAR(255)
-    status ENUM('confirmed','completed','cancelled')
-    notes TEXT
-    installation_price DECIMAL(10,2)
+    id INT PRIMARY KEY,
+    order_id INT NOT NULL,
+    technician_id INT NOT NULL,
+    scheduled_at DATETIME,
+    address VARCHAR(255),
+    status ENUM('confirmed','completed','cancelled'),
+    notes TEXT,
+    installation_price DECIMAL(10,2),
     created_at TIMESTAMP
-)
+)ENGINE=InnoDB;
